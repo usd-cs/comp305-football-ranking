a6b520adb708ec4431ba2e0707f32325892d2930
Jennifer Olsen <jenniferolsen@sandiego.edu>
2024-02-20 12:17:45 -0800

    feat: football team

---
+++ src/main/java/edu/sandiego/comp305/sp24/FootballTeam.java
@@ -0,0 +1,84 @@
+package edu.sandiego.comp305.sp24;
+
+public class FootballTeam {
+    private int wins;
+    private int ties;
+    private int losses;
+    private int goalsScored;
+    private int goalsAgainst;
+
+    public FootballTeam(int wins, int ties, int losses, int goalsScored, int goalsAgainst) {
+        this.wins = wins;
+        this.ties = ties;
+        this.losses = losses;
+        this.goalsScored = goalsScored;
+        this.goalsAgainst = goalsAgainst;
+    }
+
+    public int compareRank(FootballTeam otherTeam) {
+        int ourPoints = this.calculatePoints();
+        int otherPoints = otherTeam.calculatePoints();
+        if (ourPoints < otherPoints) {
+            return -1;
+        } else if (ourPoints > otherPoints) {
+            return 1;
+        } else {
+            return checkPointDifferential(otherTeam);
+        }
+
+    }
+
+    private int checkPointDifferential(FootballTeam otherTeam) {
+        int ourPointDifferential = this.getPointDifferential();
+        int otherPointDifferential = otherTeam.getPointDifferential();
+        if (ourPointDifferential < otherPointDifferential) {
+            return -1;
+        } else if (ourPointDifferential > otherPointDifferential) {
+            return 1;
+        } else {
+            return checkGoalsScored(otherTeam);
+        }
+    }
+
+    private int checkGoalsScored(FootballTeam otherTeam) {
+        if (this.goalsScored < otherTeam.getGoalsScored()) {
+            return -1;
+        } else if (this.goalsScored > otherTeam.getGoalsScored()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int calculatePoints() {
+        return this.wins * 3 + this.ties * 1 + this.losses * 0;
+    }
+
+    public int getPointDifferential() {
+        return this.goalsScored - this.goalsAgainst;
+    }
+    public int getWins() {
+        return wins;
+    }
+
+    public int getTies() {
+        return ties;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public int getGoalsScored() {
+        return goalsScored;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+
+
+
+
+}
